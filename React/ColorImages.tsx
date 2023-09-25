5d8a8094a8689012ea7efe6de3c0f14d761f7487
Ming Chen <cmink2040@gmail.com>
2023-09-25 13:30:35 +0900

    Moved code and added licenses.

---
+++ React/ColorImages.tsx
@@ -0,0 +1,6 @@
+import data from './UrlsADir'
+
+// Light Mode imports
+const gicon = data["url"] + data["google-icons"]  + "/" + data["color-mode"];
+
+
---
+++ React/Entry.tsx
@@ -0,0 +1,48 @@
+import data from './UrlsADir';
+
+import React from 'react';
+
+const gicon = data["url"] + data["google-icons"]  + "/";
+
+const skyai = data["url"] + data["social-icons"] + "/" + data["skyai"]+"/";
+
+const from_bin = {
+        "gicon":  data["google-icons"],
+        "skyai":  data["social-icons"] + "/" + data["skyai"]
+}
+
+
+interface EntryProps {
+    name: string;
+    location: string;
+    mode: string;
+    type: string;
+
+
+    className?: string;
+}
+
+const LDMode = {
+        'light': data['light-mode'],
+        'dark': data['dark-mode']
+}
+
+const FileTypes = {
+        'svg': '.svg',
+        'png': '.png',
+        'jpg': '.jpg',
+        'jpeg': '.jpeg',
+        'gif': '.gif',
+}
+
+
+const Entry = (props:EntryProps) => {
+    const src = data["url"]+from_bin[props.location] + "/" + LDMode[props.mode]+"/"+ props.name + FileTypes[props.type];
+
+    return <img src={src} className={props.className} 
+            alt={`Could not find: ${props.name} at ${props.location}`}/>
+    
+   
+}
+
+export default Entry;
