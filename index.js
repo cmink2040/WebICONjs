a41ed8f5e397a83a5e522afb4d4c4c45240e319e
Ming Chen <cmink2040@gmail.com>
2023-10-04 03:28:48 +0900

    Fixed the the rollup and dist, now bundles correctly and functionally. Will be testing ability to work with NPM packages via NPM Link

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const index = require('./dist/index.js')
+const data = require('./dist/data.js')
+const JS_Entry = require('./dist/JS-Entry.js')
+const React_Entry = require('./dist/React-Entry.js')
+
+module.exports = {
+    index,
+    data,
+    JS_Entry,
+    React_Entry
+}
+
+
--- src/data.ts
+++ src/data.ts
@@ -1,3 +1,11 @@
-// console.log(__dirname + "/webicon-config.js")
+const path = require('path');
 
-export const data = require( __dirname + "/webicon-config.js");
+// Access a directory in the project root using process.cwd()
+const rootDirectoryPath = process.cwd();
+
+// Construct a path relative to the project root
+const filePath = path.join(rootDirectoryPath, 'webicon-config.js');
+
+console.log(filePath)
+
+export const data = require( filePath);
